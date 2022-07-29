e2dbc09ed82a65c607ea61832b06bece8b27259d
mpolk <dmitriy.stepanenko@gmail.com>
2022-07-29 15:40:53 +0300

    Starting-trying

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" width="120" src="./assets/vite.svg" /> 
+  <img alt="Vue logo" width="120" src="./assets/logo.png" />
+  <img alt="Vue logo" width="120" src="./assets/electron.png" />
+  <img alt="Vue logo" width="120" src="./assets/ts.png" />
+  <HelloWorld msg="Hello Vite + Vue 3 + Electron + TypeScript" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img {
+  margin: 3px;
+}
+</style>
