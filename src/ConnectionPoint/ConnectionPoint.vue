f767d4a2a25f7ffd614055bb8367ebfb91907bec
mpolk <dmitriy.stepanenko@gmail.com>
2022-11-11 22:32:15 +0200

    Fixed component scaling transform implementation

---
+++ src/ConnectionPoint/ConnectionPoint.vue
@@ -0,0 +1,21 @@
+<!-- **************************************************************************>
+ *                            👑 KresMer 👑
+ *       "Kreslennya Merezh" - network diagram editor and viewer
+ *      Copyright (C) 2022 Dmitriy Stepanenko. All Rights Reserved.
+ * --------------------------------------------------------------------------
+ * A Vue component for rendering Network Connection Points 
+<*************************************************************************** -->
+
+<script setup lang="ts">
+
+    const props = defineProps({
+        name: {type: String, required: true},
+        x: {type: Number, default: 0},
+        y: {type: Number, default: 0},
+        r: {type: Number, default: 0},
+    });
+</script>
+
+<template>
+    <circle :x="x" :y="y" :r="r" style="cursor: pointer" />
+</template>
