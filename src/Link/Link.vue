4e984115d0991c504016900cee105163d2684281
mpolk <dmitriy.stepanenko@gmail.com>
2022-12-07 00:50:43 +0200

    Starting to implement Network Links

---
+++ src/Link/Link.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- **************************************************************************>
+ *                            👑 KresMer 👑
+ *       "Kreslennya Merezh" - network diagram editor and viewer
+ *      Copyright (C) 2022 Dmitriy Stepanenko. All Rights Reserved.
+ * --------------------------------------------------------------------------
+ * Network Component - a generic network element instance 
+<*************************************************************************** -->
+
+<script setup lang="ts">
+</script>
