ac876543dfe494fed01bc6879b376679a214f646
mpolk <dmitriy.stepanenko@gmail.com>
2022-12-16 16:31:26 +0200

    Implementing context menus and other app commands

--- src/electron/ipc-renderer.d.ts
+++ src/electron/ipc-renderer.d.ts
@@ -7,12 +7,12 @@
  ***************************************************************************/
 
 import {IpcRendererEvent} from 'electron';
-import { ContextMenuID } from './main/menus';
+import { ContextMenuCommands, ContextMenuID } from './main/menus';
 
 export interface IElectronAPI {
     signalReadiness: (stage: number) => void,
     onCommand: (callback: (event: IpcRendererEvent, command: string, ...args: unknown[]) => void) => void,
-    showContextMenu: (menuID: ContextMenuID, ...args: unknown[]) => void,
+    showContextMenu: <MenuID extends ContextMenuID>(menuID: MenuID, ...args: Parameters<ContextMenuCommands[MenuID]>) => void,
 }//IElectronAPI
 
 declare global {
