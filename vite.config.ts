2e7b8967823b1af2558e0bdad2c8391644d7a501
mpolk <dmitriy.stepanenko@gmail.com>
2023-01-20 17:35:37 +0200

    Separating the Electron app form the core Kresmer component

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,49 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), dts()],
-  base: './',
+export default defineConfig(({command, mode}) => {
+  // https://vitejs.dev/config/
+  const config: UserConfig = {
+    plugins: [vue(), dts()],
+    base: './',
 
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
+    resolve: {
+      alias: {
+        vue: "vue/dist/vue.esm-bundler.js",
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      }
+    },
 
-  build: {
-    lib: {
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // Add external deps here
+          globals: {
+            vue: 'Vue',
+          },
+        },
+      }
+    }  
+  };
+
+  const libMode = command === "build";
+
+  if (libMode) {
+    config.build.lib = {
       entry: path.resolve(__dirname, 'src/Kresmer.ts'),
       name: 'Kresmer',
       fileName: (format) => `kresmer.${format}.js`,
-    },
+    };
+  } else {
+    config.build.rollupOptions.input = {
+      app: "./index.electron.html"
+    };
+  }//if
 
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // Add external deps here
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    }
-  }  
-})
+  return config;
+});
