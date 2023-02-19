f5326d4292e6af783db024f16ca2ca1069d60b72
mpolk <dmitriy.stepanenko@gmail.com>
2023-02-19 23:33:33 +0200

    Fixed form submission in DrawingMerge dialog

--- src/renderer/DrawingMergeDialog.vue
+++ src/renderer/DrawingMergeDialog.vue
@@ -77,49 +77,49 @@
 <template>
     <div class="modal fade" tabindex="-1" ref="rootDiv">
         <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title fs-5">Drawing merge options...</h5>
-                <button type="button" class="btn-close" @click="close(null)"></button>
-            </div>
-            <div class="modal-body">
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" id="rbErasePreviousContent" name="mergeOptions"
-                           @click="setResult('erase-previous-content')">
-                    <label class="form-check-label" for="rbErasePreviousContent">
-                        Erase previous content
-                    </label>
+            <form class="modal-content" @submit.prevent="">
+                <div class="modal-header">
+                    <h5 class="modal-title fs-5">Drawing merge options...</h5>
+                    <button type="button" class="btn-close" @click="close(null)"></button>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" id="rbMergeDuplicates" name="mergeOptions"
-                           @click="setResult('merge-duplicates')">
-                    <label class="form-check-label" for="rbMergeDuplicates">
-                        Merge duplicates
-                    </label>
+                <div class="modal-body">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" id="rbErasePreviousContent" name="mergeOptions"
+                            @click="setResult('erase-previous-content')">
+                        <label class="form-check-label" for="rbErasePreviousContent">
+                            Erase previous content
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" id="rbMergeDuplicates" name="mergeOptions"
+                            @click="setResult('merge-duplicates')">
+                        <label class="form-check-label" for="rbMergeDuplicates">
+                            Merge duplicates
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" id="rbRenameDuplicates" name="mergeOptions"
+                            @click="setResult('rename-duplicates')">
+                        <label class="form-check-label" for="rbRenameDuplicates">
+                            Rename duplicates
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" id="rbIgnoreDuplicates" name="mergeOptions"
+                            @click="setResult('ignore-duplicates')">
+                        <label class="form-check-label" for="rbIgnoreDuplicates">
+                            Ignore duplicates
+                        </label>
+                    </div>
+                    <div v-if="showWarning" class="text-warning text-center">
+                        Please choose one of the options above
+                    </div>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" id="rbRenameDuplicates" name="mergeOptions"
-                           @click="setResult('rename-duplicates')">
-                    <label class="form-check-label" for="rbRenameDuplicates">
-                        Rename duplicates
-                    </label>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary" ref="btnOk" @click="submit">Ok</button>
+                    <button type="button" class="btn btn-secondary" @click="close(null)">Cancel</button>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" id="rbIgnoreDuplicates" name="mergeOptions"
-                           @click="setResult('ignore-duplicates')">
-                    <label class="form-check-label" for="rbIgnoreDuplicates">
-                        Ignore duplicates
-                    </label>
-                </div>
-                <div v-if="showWarning" class="text-warning text-center">
-                    Please choose one of the options above
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary" ref="btnOk" @click="submit">Ok</button>
-                <button type="button" class="btn btn-secondary" @click="close(null)">Cancel</button>
-            </div>
-            </div>
+            </form>
         </div>
     </div>
 </template>
