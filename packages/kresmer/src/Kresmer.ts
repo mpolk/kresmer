f0b6664dccaf6cbf1efa644d1431eb736403c491
mpolk <dmitriy.stepanenko@gmail.com>
2023-03-17 14:12:33 +0200

    Fix in named slots for the embedded components

--- packages/kresmer/src/Kresmer.ts
+++ packages/kresmer/src/Kresmer.ts
@@ -243,8 +243,8 @@ export default class Kresmer extends KresmerEventHooks {
                 <NetworkComponentAdapter component-class="${componentClass.name}" 
                                          :x="x" :y="y" :transform="transform" :transform-origin="transformOrigin">
                     <component :is="'${componentClass.vueName}'" v-bind="componentProps">
-                        <template v-for="(slot, slotName) in $slots" v-slot:[slotName]>
-                            <slot :name="slotName"/>
+                        <template v-for="(slot, slotName) in $slots" v-slot:[slotName]="{...props}">
+                            <slot :name="slotName" v-bind="props"/>
                         </template>
                     </component>
                 </NetworkComponentAdapter>`,
