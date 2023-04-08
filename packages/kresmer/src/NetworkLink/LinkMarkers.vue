26c805ceeee63e48607658e8326a4937e33c82f5
mpolk <dmitriy.stepanenko@gmail.com>
2023-04-08 20:02:17 +0300

    Making a "real" drawing of our network core

--- packages/kresmer/src/NetworkLink/LinkMarkers.vue
+++ packages/kresmer/src/NetworkLink/LinkMarkers.vue
@@ -12,6 +12,9 @@
 
 <template>
     <marker :id="linkMarkers.circle" markerWidth="3" markerHeight="3" refX="5" refY="5" viewBox="0 0 10 10">
-        <circle r="5" cx="5" cy="5" fill="context-stroke" stroke="context-stroke" class="marker"/>
+        <circle r="5" cx="5" cy="5" fill="context-stroke" stroke="context-stroke" />
+    </marker>
+    <marker :id="linkMarkers.arrow" markerWidth="4" markerHeight="3" refX="2" refY="5" viewBox="0 0 10 10" orient="auto">
+        <polygon points="0,10 0,0 5,0" fill="context-stroke" stroke="context-stroke"/>
     </marker>
 </template>
