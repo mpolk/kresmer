bddd5d3efbb8bc838a0b6b6df924a651e81b696a
mpolk <dmitriy.stepanenko@gmail.com>
2023-08-20 16:10:53 +0300

    Making a template for the path-panels

--- src/main-dev.ts
+++ src/main-dev.ts
@@ -11,7 +11,7 @@ import Kresmer, {NetworkComponent, NetworkComponentClass} from 'kresmer';
 const kresmer = new Kresmer('#kresmer');
 
 async function init() {
-    for (const libPath of ["stdlib.krel", "lib/kresmer-art.krel", "lib/extreme.krel", "lib/cisco.krel", "lib/juniper.krel", "lib/servers.krel"]) {
+    for (const libPath of ["stdlib.krel", "lib/kresmer-art.krel", "lib/extreme.krel", "lib/cisco.krel", "lib/juniper.krel", "lib/servers.krel", "lib/patch-panels.krel"]) {
         const lib = await (await fetch(libPath)).text();
         kresmer.loadLibrary(lib!);
     }//for
