87e70398e7e339b6f6ca6a0acd5206721fab8eb0
mpolk <dmitriy.stepanenko@gmail.com>
2023-08-21 13:00:11 +0300

    Making network core drawing

--- packages/kresmer/src/NetworkLink/LinkVertex.vue
+++ packages/kresmer/src/NetworkLink/LinkVertex.vue
@@ -18,7 +18,7 @@
 </script>
 
 <script setup lang="ts">
-    const linkNumberOffset = 6; //Tunable parameters
+    const linkNumberOffset = 4; //Tunable parameters
 
     const props = defineProps({
         model: {type: Object as PropType<LinkVertex>, required: true},
