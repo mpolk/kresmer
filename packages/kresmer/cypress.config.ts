a5c6aa06dec3b881b6c4dd2014672a1f6f183d38
mpolk <dmitriy.stepanenko@gmail.com>
2024-04-30 14:27:24 +0300

    Starting to leverage Cypress component testing

--- packages/kresmer/cypress.config.ts
+++ packages/kresmer/cypress.config.ts
@@ -1,32 +1,16 @@
 import { defineConfig } from "cypress";
-import * as fs from "node:fs";
-import * as path from "node:path";
+import tasks from "./cypress/support/tasks.config";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "vite",
-    }, //devServer
+    component: {
+        devServer: {
+            framework: "vue",
+            bundler: "vite",
+        }, //devServer
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {
-      on('task', {
-        loadLibraries() {
-          const libDir = "./lib";
-          const libs = {};
-          const libFilePaths = fs.readdirSync(libDir);
-          libFilePaths.forEach(filePath => {
-            const matches = filePath.match(/(.*)\.krel$/);
-            if (matches) {
-              const libName = matches[1];
-              const libData = fs.readFileSync(path.join(libDir, filePath), "utf8");
-              libs[libName] = libData;
-            }//if
-          });
-          return libs;
-        },//loadLibraries
-      })//on task
-    },//setupNodeEvents
-  }//component
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        setupNodeEvents(on, config) {
+            on('task', tasks);
+        },//setupNodeEvents
+    }//component
 });
---
+++ packages/kresmer/cypress/support/tasks.config.ts
@@ -0,0 +1,27 @@
+/***************************************************************************\
+ *                            👑 KresMer 👑
+ *       "Kreslennya Merezh" - network diagram editor and viewer
+ *      Copyright (C) 2022-2024 Dmitriy Stepanenko. All Rights Reserved.
+ * -----------------------------------------------------------------------
+ * Test support functions exposed as Cypress tasks
+ ***************************************************************************/
+
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+export default {
+    loadLibraries() {
+        const libDir = "./lib";
+        const libs: Record<string, string> = {};
+        const libFilePaths = fs.readdirSync(libDir);
+        libFilePaths.forEach(filePath => {
+            const matches = filePath.match(/(.*)\.krel$/);
+            if (matches) {
+                const libName = matches[1];
+                const libData = fs.readFileSync(path.join(libDir, filePath), "utf8");
+                libs[libName] = libData;
+            }//if
+        });
+        return libs;
+    },//loadLibraries
+}//export
