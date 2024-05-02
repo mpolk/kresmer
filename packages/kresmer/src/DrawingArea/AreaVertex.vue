dd3e7ba24f1ef5b8d67ed95399c6424a0518e3a8
mpolk <dmitriy.stepanenko@gmail.com>
2024-05-02 13:17:37 +0300

    Implementing DrawingAreas

---
+++ packages/kresmer/src/DrawingArea/AreaVertex.vue
@@ -0,0 +1,28 @@
+<!-- **************************************************************************>
+ *                            👑 KresMer 👑
+ *       "Kreslennya Merezh" - network diagram editor and viewer
+ *      Copyright (C) 2022-2024 Dmitriy Stepanenko. All Rights Reserved.
+ * --------------------------------------------------------------------------
+ * DrawingArea Vertex - presentation code 
+<*************************************************************************** -->
+<script lang="ts">
+    import { PropType } from 'vue';
+    import AreaVertex from './AreaVertex';
+    import BaseVertexVue from '../Vertex/BaseVertex.vue';
+
+    export default {
+        components: {BaseVertexVue},
+    }
+</script>
+
+<script setup lang="ts">
+
+    defineProps({
+        model: {type: Object as PropType<AreaVertex>, required: true},
+    });
+    
+</script>
+
+<template>
+    <BaseVertexVue :model="model" />
+</template>
