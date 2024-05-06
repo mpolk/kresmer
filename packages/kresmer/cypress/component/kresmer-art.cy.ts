252ad88ecd3b30caf716e8d94e6aa98aa402e8aa
mpolk <dmitriy.stepanenko@gmail.com>
2024-05-06 12:46:40 +0300

    Reorganize test files

--- packages/kresmer/cypress/component/kresmer-art.cy.ts
+++ packages/kresmer/cypress/component/kresmer-art.cy.ts
@@ -18,49 +18,58 @@ describe('Kresmer Art', () => {
         });
     });
 
-    it('loads a "Kresmer Art" drawing', () => {
-        cy.readFile("../../kresmer-art.kre").then(async (drawingData) => {
-            await kresmer.loadDrawing(drawingData);
-            kresmer
-                .registerNetworkComponentClass(new NetworkComponentClass("GoldenKresmer", {
-                    template: `
-                        <rect x="0" y="0" :width="width" :height="height" 
-                                stroke="gold" stroke-width="8px" stroke-opacity="0.5"/>
-                        <text :x="width*0.25" :y="height*0.6" stroke="gold" :font-size="fontSize">{{text}}</text>
-                        <Kre:Crown  x="10" v-bind:y="height*0.6" v-bind:font-size="fontSize"/>
-                        <text :x="width*0.95" :y="height*0.6" fill="gold" :font-size="fontSize" text-anchor="end">
-                        <template v-for="i in 3">⚜</template></text>
-                    `,
+    it('Load an additional "Kresmer Art" library', () => {
+        cy.fixture("kresmer-art.krel").then(libData => {
+            kresmer.loadLibrary(libData);
+        })
+    })
+
+    it('Load a "Kresmer Art" drawing', () => {
+        cy.fixture("kresmer-art.kre").then((drawingData) => {
+            kresmer.loadDrawing(drawingData);
+        })
+    })
+
+    it("...and add one component programmatically", () => {
+        kresmer
+            .registerNetworkComponentClass(new NetworkComponentClass("GoldenKresmer", {
+                template: `
+                    <rect x="0" y="0" :width="width" :height="height" 
+                            stroke="gold" stroke-width="8px" stroke-opacity="0.5"/>
+                    <text :x="width*0.25" :y="height*0.6" stroke="gold" :font-size="fontSize">{{text}}</text>
+                    <Kre:Crown  x="10" v-bind:y="height*0.6" v-bind:font-size="fontSize"/>
+                    <text :x="width*0.95" :y="height*0.6" fill="gold" :font-size="fontSize" text-anchor="end">
+                    <template v-for="i in 3">⚜</template></text>
+                `,
+                props: {
+                    width: {type: Number, required: true},
+                    height: {type: Number, required: true},
+                    fill: {type: String, default: "yellow"},
+                    text: {type: String},
+                    fontSize: {type: String},
+                },
+            }))
+            .placeNetworkComponent(new NetworkComponent(kresmer, "GoldenKresmer", {
                     props: {
-                        width: {type: Number, required: true},
-                        height: {type: Number, required: true},
-                        fill: {type: String, default: "yellow"},
-                        text: {type: String},
-                        fontSize: {type: String},
-                    },
-                }))
-                .placeNetworkComponent(new NetworkComponent(kresmer, "GoldenKresmer", {
-                        props: {
-                            width: 550,
-                            height: 50,
-                            text: "Golden Kresmer",
-                            fontSize: "32"
-                        }
-                    }), 
-                    {x: 100, y: 50}
-                )
-        });
+                        width: 550,
+                        height: 50,
+                        text: "Golden Kresmer",
+                        fontSize: "32"
+                    }
+                }), 
+                {x: 100, y: 50}
+            )
 
         cy.get("svg.GoldenKresmer").should("exist");
         cy.get("svg.SilverKresmer").should("exist");
     });
 
-    it('shows a grid', function() {
+    it('Show a grid', function() {
         kresmer.showGrid = true;
         cy.get("line.grid").should("exist");
     });
 
-    it('shows rulers', function() {
+    it('Show rulers', function() {
         kresmer.showRulers = true;
         cy.get("rect.axis").should("exist");
         cy.get("line.marking").should("exist");
